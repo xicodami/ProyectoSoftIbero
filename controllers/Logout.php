fb20cc7847d8dd3be30ad1c96607a82cf5fc3ee5
Taxxa2024 <proyectos@taxxa.co>
2025-03-24 18:06:52 -0500

    Archivo Index

--- controllers/Logout.php
+++
@@ -1,9 +0,0 @@
-<?php session_start();
-    class Logout{
-        public function __construct(){}
-        # --  Cerrar Sesión ----//
-        public function index(){
-            session_destroy();
-            header("Location: ?");
-        }
-    }
