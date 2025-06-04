021eb1a2599a38e93a04ac6c75aedc2a06f10497
DIEGO-DEVESOFT <diegogonzalezprogramador@gmail.com>
2025-06-03 21:49:20 -0500

    Actualizacion POS

--- vistas/modulos/footer.php
+++ vistas/modulos/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
 	
-	<strong>Copyright &copy; 2025<a href="" target="_blank">OnliFact</a>.</strong>
+	<strong>Copyright &copy; 2025 <a href="https://www.OnlyFact.com" target="_blank">OnlyFact</a>.</strong>
 
 	Todos los derechos reservados.
 
